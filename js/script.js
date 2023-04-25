14dc1b89236fa43e1a07959b5ad2567192dedc02
isabellerod <isabeller29@hotmail.com>
2023-04-25 11:47:29 -0300

    img galery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const links = document.querySelectorAll('.header-menu a');
 const parametros = new URLSearchParams(location.search);
 const perguntas = document.querySelectorAll('.perguntas button');
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
 
 function ativarLink(link){
     const url = location.href;
@@ -34,6 +36,20 @@ function eventosPerguntas(pergunta){
     pergunta.addEventListener('click', ativarPergunta);
 }
 
+function trocarImagem(event){
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+
+    if (media){
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img){
+    img.addEventListener('click', trocarImagem);
+}
+
 links.forEach(ativarLink);
 parametros.forEach(ativarProduto);
 perguntas.forEach(eventosPerguntas);
+galeria.forEach(eventosGaleria);
