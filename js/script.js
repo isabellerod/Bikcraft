af5a89dad1011e358de9b518a3ba558e2acdff5b
isabellerod <isabeller29@hotmail.com>
2023-04-25 11:04:39 -0300

    url parameters

--- js/script.js
+++ js/script.js
@@ -10,3 +10,15 @@ function ativarLink(link){
 }
 
 links.forEach(ativarLink);
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro);
+
+    if(elemento){
+    elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
